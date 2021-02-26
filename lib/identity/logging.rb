c97012dede45b49459db2a7a65ee14f1ecb2dd89
Zach Margolis <zachary.margolis@gsa.gov>
2021-02-26 10:16:47 -0800

    bundle gem --no-exe --no-ext --test=rspec identity-logging

---
+++ lib/identity/logging.rb
@@ -0,0 +1,8 @@
+require "identity/logging/version"
+
+module Identity
+  module Logging
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/identity/logging_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Identity::Logging do
+  it "has a version number" do
+    expect(Identity::Logging::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
